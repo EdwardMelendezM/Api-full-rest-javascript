1cd3e5aca2811ee60022eb570e91318b60f1701c
EdwardMelendezM <106676335+EdwardMelendezM@users.noreply.github.com>
2023-02-14 23:41:49 -0500

    Agregando cambios en el manejo  de errores

--- controllers/storage.js
+++ controllers/storage.js
@@ -1,5 +1,7 @@
 const PUBLIC_URL = process.env.PUBLIC_URL;
+const { matchedData } = require("express-validator");
 const { storageModel } = require("../models");
+const { handleError } = require("../utils/handleError");
 
 const createItem = async (req, res) => {
   const { body, file } = req
@@ -11,6 +13,33 @@ const createItem = async (req, res) => {
   const data = await storageModel.create(fileData);
   res.send({ menssage: "POST_COMPLETED", fileData })
 }
+const getItems = async (req, res) => {
+  try {
+    const data = await storageModel.find({});
+    res.send(data)
+  } catch (error) {
+    handleError(res, "ERROR_GET_ITEMS")
+  }
+}
+const getItem = async (req, res) => {
+  try {
+    const { id } = matchedData(req)
+    const data = await storageModel.findById(id);
+    res.send(data)
+  } catch (error) {
+    handleError(res, "ERROR_GET_ITEM_DETAILS")
+  }
+}
+
+const deleteItem = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await storageModel.delete(id);
+    res.send(data)
+  } catch (error) {
+    handleError(res, "ERROR_GET_ITEMS")
+  }
+}
 
 
-module.exports = { createItem }
+module.exports = { createItem, getItems, getItem, deleteItem }
--- routes/storage.js
+++ routes/storage.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const { createItem } = require('../controllers/storage');
+const { createItem, getItem, getItems, deleteItem } = require('../controllers/storage');
 const uploadMiddleware = require('../utils/handleStorage');
+const { validatorGetItem } = require('../validators/storage');
 const router = express.Router();
 
 router.post("/", uploadMiddleware.single("myfile"), createItem)
+router.get("/", getItems)
+router.get("/:id", validatorGetItem, getItem)
+router.delete("/:id", validatorGetItem, deleteItem)
 
 
 module.exports = router;
